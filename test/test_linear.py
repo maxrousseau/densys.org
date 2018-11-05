8ffe2bd0b1b02334dcb160298eb98708c5fbbee9
maxrousseau <maximerousseau08@gmail.com>
2018-11-04 19:54:42 -0500

    files added

---
+++ test/test_linear.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import sys
+sys.path.append("../")
+import os
+import pytest
+from modules import linear
+
+class TestLinear(object):
+    """Testing for the Linear class
+
+    This object will be testing the Linear class and its associated methods.
+
+    This class contains methods that will mirror those of the Linear class and
+    assert their functionalities.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    ------
+    None
+    """
