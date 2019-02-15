f037f8af368f081606d2440a4b1eae11076eb28a
maxrousseau <maximerousseau08@gmail.com>
2019-02-15 16:30:06 -0500

    modified to silent curl

--- api-test.sh
+++ api-test.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+# TODO:
+# - add time variable for api calls and timeouts
+
 echo "[INFO] local host api call"
 
-curl -g -i -H Content-Type:application/json -X POST -d  \
+curl -s -g -i -H Content-Type:application/json -X POST -d  \
 	'{"image":"http://optipng.sourceforge.net/pngtech/img/lena.png",
 	"analysis":"asym"}' \
 	http://localhost:5000/api/v0.0/jobs/new > local_api_test.log
@@ -16,7 +19,7 @@ fi
 
 echo "[INFO] heroku server api call"
 
-curl -g -i -H Content-Type:application/json -X POST -d \
+curl -s -g -i -H Content-Type:application/json -X POST -d \
 	'{"image":"http://optipng.sourceforge.net/pngtech/img/lena.png",
 	"analysis":"asym"}' \
 	https://stormy-hollows-38428.herokuapp.com/api/v0.0/jobs/new \
@@ -24,8 +27,8 @@ curl -g -i -H Content-Type:application/json -X POST -d \
 
 if grep -q "BAD" server_api_test.log;
 then
-	echo "[WARNING] local api call error"
-	echo "[INFO] view call output in local_api_test.log"
+	echo "[WARNING] heroku server api call error"
+	echo "[INFO] view call output in server_api_test.log"
 else
-	echo "[INFO] local api call completed without error"
+	echo "[INFO] heroku server api call completed without error"
 fi
