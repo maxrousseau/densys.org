0e77134d80a59e2e5f2ceaed14045bb2938cebed
maxrousseau <maximerousseau08@gmail.com>
2019-02-15 16:21:48 -0500

    if statement modification for error catching

--- api-test.sh
+++ api-test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 echo "[INFO] local host api call"
 
-curl -g -i -H Content-Type:application/json -X POST -d \
+curl -g -i -H Content-Type:application/json -X POST -d  \
 	'{"image":"http://optipng.sourceforge.net/pngtech/img/lena.png",
 	"analysis":"asym"}' \
 	http://localhost:5000/api/v0.0/jobs/new > local_api_test.log
 
-if grep "error" local_api_test.log;
+if grep -q "BAD" local_api_test.log;
 then
 	echo "[WARNING] local api call error"
 	echo "[INFO] view call output in local_api_test.log"
@@ -22,7 +22,7 @@ curl -g -i -H Content-Type:application/json -X POST -d \
 	https://stormy-hollows-38428.herokuapp.com/api/v0.0/jobs/new \
 	> server_api_test.log
 
-if grep "error" server_api_test.log;
+if grep -q "BAD" server_api_test.log;
 then
 	echo "[WARNING] local api call error"
 	echo "[INFO] view call output in local_api_test.log"
