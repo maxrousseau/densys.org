ca611a5c2196017419cf27adb1db1db18dcbc14e
maxrousseau <maximerousseau08@gmail.com>
2019-02-18 20:20:58 -0500

    api test script assertion of success modified, now working properly

--- api-test.sh
+++ api-test.sh
@@ -9,12 +9,12 @@ curl -s -g -i -H Content-Type:application/json -X POST -d  \
 	"analysis":"asym"}' \
 	http://localhost:5000/api/v0.0/jobs/new > local_api_test.log
 
-if grep -q "BAD" local_api_test.log;
+if [ $? -eq 0 ];
 then
+	echo "[INFO] local api call completed without error"
+else
 	echo "[WARNING] local api call error"
 	echo "[INFO] view call output in local_api_test.log"
-else
-	echo "[INFO] local api call completed without error"
 fi
 
 echo "[INFO] heroku server api call"
@@ -25,10 +25,10 @@ curl -s -g -i -H Content-Type:application/json -X POST -d \
 	https://stormy-hollows-38428.herokuapp.com/api/v0.0/jobs/new \
 	> server_api_test.log
 
-if grep -q "BAD" server_api_test.log;
+if [ $? -eq 0 ];
 then
+	echo "[INFO] heroku server api call completed without error"
+else
 	echo "[WARNING] heroku server api call error"
 	echo "[INFO] view call output in server_api_test.log"
-else
-	echo "[INFO] heroku server api call completed without error"
 fi
